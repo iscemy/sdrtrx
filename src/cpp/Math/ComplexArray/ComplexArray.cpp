67978eadf5dee360fc0568dec2741c6381771487
cem <you@example.com>
2023-07-30 12:33:14 +0300

    Revert "fixed memory leak"
    This reverts commit c019d4df355c5317bbff7979f491b194baff4f9e.

--- src/cpp/Math/ComplexArray/ComplexArray.cpp
+++ src/cpp/Math/ComplexArray/ComplexArray.cpp
@@ -128,10 +128,8 @@ ComplexArray ComplexArray::GetConjugate() {
 
 ComplexArray::~ComplexArray()
 {
-    if (isBufferOwnedByThis && isBufferInited){
+    if (isBufferOwnedByThis && isBufferInited)
         delete buffer;
-        delete ptemp;    
-    }
 }
 
 void ComplexArray::SetBuffer(int16_t *start, int inputSize, int step) {
